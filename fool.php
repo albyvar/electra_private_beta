3f8c3d4fc6f3b6ae565f9c2d0b2aa08ca8182c74
alby_var <builderstaff11@libero.it>
2018-06-24 18:02:18 +0100

    Delete fool.php

--- fool.php
+++
@@ -1,8 +0,0 @@
-<html>
- <head>
-  <title>Error</title>
- </head>
- <body>
- <?php echo "Error! You need a fucking life and wait for Electra to be released officially by coolstar!<p>"; ?>
- </body>
-</html>
