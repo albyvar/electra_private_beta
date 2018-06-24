d98dcef84e6a87cd70eb770060c0ab3c0dcaa1a0
alby_var <builderstaff11@libero.it>
2018-06-24 18:56:04 +0200

    Add files via upload

---
+++ fool.php
@@ -0,0 +1,8 @@
+<html>
+ <head>
+  <title>Error</title>
+ </head>
+ <body>
+ <?php echo "Error! You need a fucking life and wait for Electra to be released officially by coolstar!<p>"; ?>
+ </body>
+</html>
